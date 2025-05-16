be11d9868118d6316441fa159709c1dd16690560
Pedro <ribeiro.tpedro@gmail.com>
2025-05-16 16:56:11 -0300

    feat: create compress service

--- apps/compressor-service/src/compressor.service.ts
+++ apps/compressor-service/src/compressor.service.ts
@@ -1,8 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import path from 'path';
+import fs from 'fs'
+import sharp from 'sharp';
 
 @Injectable()
 export class CompressorService {
-  getHello(): string {
-    return 'Hello World!';
+  async compress(inputPath: string, filename: string): Promise<string[]> {
+    const baseName = path.parse(filename).name;
+    const ext = path.extname(filename).toLowerCase();
+    
+    const outputDir = path.resolve(process.cwd(), 'compressed')
+
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir, { recursive: true });
+    }
+
+    const qualities = [
+      { label: 'low', quality: 30 },
+      { label: 'medium', quality: 60 },
+      { label: 'high', quality: 85 },
+    ];
+
+    const outputPaths: string[] = [];
+
+      for (const { label, quality } of qualities) {
+      const outputFilename = `${baseName}-${label}${ext}`;
+      const outputPath = path.resolve(outputDir, outputFilename);
+
+      await sharp(inputPath)
+        .resize({ width: 1280 })
+        .jpeg({ quality })
+        .toFile(outputPath);
+
+      outputPaths.push(outputPath);
+    }
+
+    return outputPaths;
   }
 }
--- apps/compressor-service/src/main.ts
+++ apps/compressor-service/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { CompressorModule } from './compressor.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(CompressorModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    CompressorModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://localhost:5672'],
+        queue: 'image-processing',
+        queueOptions: {
+          durable: true,
+        }
+      }
+    }
+  );
+  
+  await app.listen();
+  console.log('image-compressor microservice is listening...');
 }
 bootstrap();
