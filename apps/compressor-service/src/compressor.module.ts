796978f407af242ae5fcc4bb397656cee6656902
Pedro <ribeiro.tpedro@gmail.com>
2025-05-17 13:58:59 -0300

    feat: add rabbitmq to compressor service

--- apps/compressor-service/src/compressor.module.ts
+++ apps/compressor-service/src/compressor.module.ts
@@ -2,14 +2,20 @@ import { Module } from '@nestjs/common';
 import { CompressorController } from './compressor.controller';
 import { CompressorService } from './compressor.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CompressionTask, CompressionTaskSchema } from './database/compression-task.schema';
+import {
+  CompressionTask,
+  CompressionTaskSchema,
+} from './database/compression-task.schema';
+import { RabbitMQConsumer } from './rabbitmq/rabbitmq-consumer.service';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/image-db'),
-    MongooseModule.forFeature([{ name: CompressionTask.name, schema: CompressionTaskSchema }]),
+    MongooseModule.forFeature([
+      { name: CompressionTask.name, schema: CompressionTaskSchema },
+    ]),
   ],
   controllers: [CompressorController],
-  providers: [CompressorService],
+  providers: [CompressorService, RabbitMQConsumer],
 })
 export class CompressorModule {}
